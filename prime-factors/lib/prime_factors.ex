72ce46c296a8a606fdd4aba4d87eaf4bacdf9618
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2022-08-12 07:33:14 +0200

    Prime factors: iteration #1

---
+++ prime-factors/lib/prime_factors.ex
@@ -0,0 +1,35 @@
+defmodule PrimeFactors do
+  @doc """
+  Compute the prime factors for 'number'.
+
+  The prime factors are prime numbers that when multiplied give the desired
+  number.
+
+  The prime factors of 'number' will be ordered lowest to highest.
+  """
+  @spec factors_for(pos_integer) :: [pos_integer]
+  def factors_for(num) do
+    prime_factors(num, [])
+  end
+
+  defp prime_factors(1, acc), do: Enum.reverse(acc)
+  defp prime_factors(num, acc) do
+    next = first_prime_factor(num, 2)
+    prime_factors(div(num, next), [next | acc])
+  end
+
+  defp first_prime_factor(num, factor) do
+    cond do
+      factor == 1 -> 1
+      rem(num, factor) == 0 && prime?(factor) -> factor
+      true -> first_prime_factor(num, factor + 1)
+    end
+  end
+
+  defp prime?(2), do: true
+  defp prime?(number) do
+    # TODO: Improve upper limit for efficiency
+    upper = (number |> :math.sqrt() |> trunc) + 1
+    Enum.all?((2..upper), & rem(number, &1) != 0)
+  end
+end
