7ae541fe508f21ebdc6daefa5ef2759874c4d258
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2022-09-05 19:30:59 +0200

    Badge name: iteration #2

--- name-badge/lib/name_badge.ex
+++ name-badge/lib/name_badge.ex
@@ -1,6 +1,19 @@
 defmodule NameBadge do
-  def print(nil, name, nil), do: "#{name} - OWNER"
-  def print(nil, name, department), do: "#{name} - #{String.upcase(department)}"
-  def print(id, name, nil), do: "[#{id}] - #{name} - OWNER"
-  def print(id, name, department), do: "[#{id}] - #{name} - #{String.upcase(department)}"
+  def print(id, name, department) do
+    name
+    |> add_id(id)
+    |> add_department(department)
+  end
+
+  def add_id(badge, id) do
+    if id,
+      do: "[#{id}] - " <> badge,
+      else: badge
+  end
+
+  def add_department(badge, department) do
+    if department,
+      do: badge <> " - #{String.upcase(department)}",
+      else: badge <> " - OWNER"
+  end
 end
