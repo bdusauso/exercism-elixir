f1caebfadaf0b6c661699c8df6ee51b79b0c798c
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2022-09-07 20:07:05 +0200

    Wine cellar: iteration #3

--- wine-cellar/lib/wine_cellar.ex
+++ wine-cellar/lib/wine_cellar.ex
@@ -8,20 +8,14 @@ defmodule WineCellar do
   end
 
   def filter(cellar, color, opts \\ []) do
-    wines = Keyword.get_values(cellar, color)
-
-    wines =
-      if Keyword.has_key?(opts, :year),
-        do: filter_by_year(wines, Keyword.fetch!(opts, :year)),
-        else: wines
-
-    wines =
-      if Keyword.has_key?(opts, :country),
-        do: filter_by_country(wines, Keyword.fetch!(opts, :country)),
-        else: wines
-
-    wines
+    cellar
+    |> Keyword.get_values(color)
+    |> filter_by_country(opts[:country])
+    |> filter_by_year(opts[:year])
   end
+
+  defp filter_by_year(wines, nil), do: wines
+  defp filter_by_country(wines, nil), do: wines
 
   # The functions below do not need to be modified.
 
